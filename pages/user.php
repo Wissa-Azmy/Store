ec1a24c5d0cab33d49bd8c42e271f4e2dd28e1d6
Wissa-Azmy <wisa.lfg@gmail.com>
2016-01-31 10:14:59 +0200

    Removed unused files

---
+++ pages/user.php
@@ -0,0 +1,65 @@
+<?php
+
+class admin{
+
+    var $id;
+	var $userName;
+	var $password;
+
+	public function __construct($id=-1) {
+        global $db;
+		if($id!=-1) { // To view only one product by id if it's sent with the constructor.
+			$query = "SELECT * FROM admin WHERE id=$id LIMIT 1";
+			$result = $db->query($query);
+			$admin = $db->fetch_assoc($result);
+            
+            $this->userName = $admin['username'];
+			$this->password = $admin['password'];
+		}
+
+	}
+
+	function insert() {
+        global $db;
+        $query = "SELECT * FROM admin WHERE username LIKE '$this->userName'";
+        $sql = $db->query($query);
+
+        $rows = $db->num_rows($sql);
+
+        if($rows > 0){
+            echo "Invalid, You are trying to add a user that already exists.";
+            exit();
+        }else {
+            $query = "INSERT INTO admin VALUES(NULL,'$this->userName','$this->password', now(), NULL)";
+            $db->query($query) or die("Error adding the Product");
+            return $db->insert_id();
+        }
+	}
+
+	function update() {
+        global $db;
+		$query = "UPDATE admin SET username='$this->userName', password='$this->password' where id=$this->id";
+		$db->query($query);
+	}
+
+	function delete() {
+        global $db;
+		$query = "DELETE FROM admin WHERE id=$this->id";
+		$db->query($query);
+	}
+
+	static function viewAll() {
+        global $db;
+		$query = "SELECT * FROM admin";
+		$result = $db->query($query);
+		$data = [];
+
+		while($row = $db->fetch_assoc($result)) {
+			$data[] = $row;
+		}
+		return $data;
+	}
+
+}
+
+?>
---
+++ pages/user_list.php
@@ -0,0 +1,130 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: wissa Azmy
+ * Date: 1/25/16
+ * Time: 2:20 PM
+ */
+require "../scripts/db-connect.php";
+
+require 'session_validation.php';
+
+require 'user.php';
+
+/************** ERROR REPORTING. *******************/
+error_reporting(E_ALL);  // set the php.ini config. file to dispaly all the errors
+ini_set('error_display','1');
+
+/************** VIEW PRODUCT LIST ******************/
+
+$adminList = '';
+
+$result = admin::viewAll();
+
+if(!empty($result)){
+    foreach($result as $admin){
+
+        $id = $admin['id'];
+        $admin_name= $admin['username'];
+        $adminList .= "$id- $admin_name<br/>";
+    }
+
+}else{
+    $adminList = "There are no details to be listed.";
+}
+
+/******************* ADD PRODUCT FORM PROCESSING ****************************/
+if(isset($_POST['productName'])){
+
+    $product = new product;
+
+    $product->productName = $db->escape_string($_POST['productName']);
+    $product->price = $db->escape_string($_POST['price']);
+    $product->details = $db->escape_string($_POST['details']);
+    $product->category = $db->escape_string($_POST['category']);
+    $product->subcategory = $db->escape_string($_POST['subcategory']);
+
+    $product->insert();
+    header('location:inventory_list.php');
+}
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel= "stylesheet" href="../styles/styles.css" type="text/css" />
+
+    <title>Users</title>
+</head>
+<body>
+<div align="right">
+    <a href="admin_logout.php" rel="Log out">Log out</a>
+</div>
+<div align="center" id="main">
+    <a href="#inventoryForm" class="menu">+ Add User</a>
+
+    <p>Admin Users: </p>
+    <br/>
+    <?php echo $adminList; ?>
+
+    <a name="inventoryForm" id="inventoryForm"></a>
+    <div class="form">
+        <form enctype="multipart/form-data" method="post" >
+            <h1>Product Details</h1>
+                <legend><span class="number">+</span> Product Info</legend>
+                <input type="text" name="productName" placeholder="Product Name">
+                <br />
+                <input type="text" name="price" placeholder="Price">
+                <br /><br />
+                <select name="category">
+                    <option >-- Category --</option>
+                    <optgroup label="iPhone">
+                        <option value="OS">iPhone 6 plus</option>
+                        <option value="EL">iPhone 6</option>
+                        <option value="SD">iPhone 5</option>
+                    </optgroup>
+                    <optgroup label="iPad">
+                        <option value="OS">iPad Pro</option>
+                        <option value="EL">iPad Air 2</option>
+                        <option value="SD">iPad Air</option>
+                    </optgroup>
+                    <optgroup label="MacBook">
+                        <option value="OS">MacBook Pro</option>
+                        <option value="EL">MacBook Air</option>
+                        <option value="SD">New MacBook</option>
+                    </optgroup>
+                </select>
+                <select name="subcategory">
+                    <option >-- SubCategory --</option>
+                    <optgroup label="iPhone">
+                        <option value="OS">iPhone 6 plus</option>
+                        <option value="EL">iPhone 6</option>
+                        <option value="SD">iPhone 5</option>
+                    </optgroup>
+                    <optgroup label="iPad">
+                        <option value="OS">iPad Pro</option>
+                        <option value="EL">iPad Air 2</option>
+                        <option value="SD">iPad Air</option>
+                    </optgroup>
+                    <optgroup label="MacBook">
+                        <option value="OS">MacBook Pro</option>
+                        <option value="EL">MacBook Air</option>
+                        <option value="SD">New MacBook</option>
+                    </optgroup>
+                </select>
+                <input type="file" name="photo" id="photo" />
+                <br />
+                <textarea cols="10" rows="10" name="details" placeholder= "Enter product details and description here..."></textarea>
+                <br />
+
+            <br />
+
+            <button type="submit" class="button">Add Product</button>
+        </form>
+    </div>
+
+
+</div>
+</body>
+</html>
